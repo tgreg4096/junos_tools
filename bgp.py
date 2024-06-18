eaad2b5bc6a5aeb7a53eb2ab2d95186b5d7e0914
tgreg4096 <125890203+tgreg4096@users.noreply.github.com>
2024-06-18 19:46:35 +0100

    Update bgp.py

--- bgp.py
+++ bgp.py
@@ -13,17 +13,16 @@ dev.open()
 
 rpc = dev.rpc.get_bgp_summary_information()
 rpc_xml = etree.tostring(rpc, pretty_print=True, encoding='unicode')
-dev.close()
-
 
 xmlparser = jxmlease.Parser()
 result = jxmlease.parse(rpc_xml)
 
-
 for neighbor in result['bgp-information']['bgp-peer']:
-    print('Checking peer with IP address: ' + neighbor['peer-address'])
-    print(neighbor['peer-as'])
-    print(neighbor['peer-state'])
-    print(neighbor['elapsed-time'])
-    print(neighbor['input-messages'])
-    print(neighbor['output-messages'])
+    print(f"Checking peer with IP address {neighbor['peer-address']}")
+    print(f" BGP accepted prefixes:\t {neighbor['bgp-rib']['accepted-prefix-count']}")
+    print(f" BGP peer AS:\t\t {neighbor['peer-as']}")
+    print(f" BGP peer state:\t {neighbor['peer-state']}")
+    print(f" BGP session uptime:\t {neighbor['elapsed-time']}")
+    print(f" BGP input messages:\t {neighbor['input-messages']}")
+    print(f" BGP output messages:\t {neighbor['output-messages']}")
+dev.close()
